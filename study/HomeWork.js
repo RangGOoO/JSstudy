932c73c8c1067fe0da957cf45fd8839db50149bd
RangGOoO <breakking@naver.com>
2020-05-31 23:36:26 +0900

    JS-PROJECT

--- study/HomeWork.js
+++
@@ -1,7 +0,0 @@
-const btnForm = document.querySelector(".btn"),
-    Button = btnForm.getElementsByTagName("button");
-
-
-function MS_show(number){
-     alert(`${Button[number-1].textContent} Button Click`)
-}
