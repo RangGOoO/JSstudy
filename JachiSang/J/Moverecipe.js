b184e497f8e4cc64da63a1aa27a124a60a2c3551
RangGOoO <breakking@naver.com>
2020-07-02 13:01:15 +0900

    JS-PROJECT

--- JachiSang/J/Moverecipe.js
+++ JachiSang/J/Moverecipe.js
@@ -1,15 +1,26 @@
-const menuList = document.querySelectorAll('.menuList');
+const menuList = document.querySelectorAll('.menuList'), 
+    SliderBox = document.querySelector('.SliderBox');
 const iframe = document.querySelector('.iframeBox');
 
-let pageNumber = 0;
-for(let i = 0; i < menuList.length; i++){
-    for(let y = 0; y < menuList[i].children.length; y++){
-        menuList[i].children[y].addEventListener('click', function(){
-            iframe.src = `../Recipe/Easy/Easy${i}-${y}.html`;
-            console.log(iframe.src);
-        })
-        console.log(menuList[i].children[y]);
-        pageNumber++;
+function changeSrc(){
+    for(let i = 0; i < menuList.length; i++){
+        for(let y = 0; y < menuList[i].children.length; y++){
+            menuList[i].children[y].addEventListener('click', function(){
+                SliderBox.classList.remove('show');
+                SliderBox.classList.add('hide');
+                iframe.classList.remove('hide');
+                iframe.classList.add('show');
+                iframe.src = `../Recipe/Easy/Easy${i}-${y}.html`;
+            })
+        }
     }
 }
+
+function init(){
+    SliderBox.classList.add('show');
+    iframe.classList.add('hide');
+    changeSrc();
+}
+
+init();
 
